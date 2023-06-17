2f49450c3e313945a3adbc84f669516fa09be7f9
rwxp <sdrivert@hotmail.com>
2023-06-16 22:21:21 -0500

    To be continued

--- frontend-app/tests/prueba.js
+++ frontend-app/tests/prueba.js
@@ -5,6 +5,7 @@ import chrome from 'selenium-webdriver/chrome.js';
 (async function example() {
   // Configuración del navegador y la URL del componente
   const options = new chrome.Options();
+  options.addArguments('--headless');
   const url = 'http://24.199.120.226:30528/'; // La URL de tu aplicación
 
   const driver = await new Builder()
